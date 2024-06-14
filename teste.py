4ec155745fdcea60dfaacb0bb77dcd56e898f133
Ionesmar <ionesmar.de.lima.carvalho@hotmail.com>
2024-06-14 16:33:10 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,12 +1,12 @@
-def calcule_menor (lista):
-    menor=lista[0]
-    for elem in lista:
-        if elem<menor:
-            menor=elem
-    return menor
+def taximetro(distancia, multiplicador=1):
+    largada =3
+    km_rodado = 2
+    valor = (largada + distancia *
 
-lista= [5,6,3,23,6,7,8,4]
-menor_elem =calcule_menor(lista)
-print(f'O menor valor da lista é : {menor_elem}')
-'''a,b = 5,10
-print (a*b)'''
+    km_rodado) * multiplicador
+    return valor
+
+
+pagamento = taximetro(3.5)
+print(pagamento)
+
