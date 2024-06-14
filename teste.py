4e743500aad78766ece19348fea00ecae9a41382
Ionesmar <ionesmar.de.lima.carvalho@hotmail.com>
2024-06-14 14:28:46 -0300

    menor elem. da lista
    Calcule o menor elemento da lista

--- teste.py
+++ teste.py
@@ -1,15 +1,12 @@
-print( A VACINAÇÃO DE CRIANÇAS CONTRA SARAMPO OCORRE A PARTIR DE 1 ANO DE IDADE,
-IDENTIFIQUE QUANTOS ANOS TEM A CRIANÇA PARA INFORMAR A DATA DE VACINAÇÃO NO POSTO MAIS PRÓXIMO
-DE SUA RESIDÊNCIA, A VACINA ESTA COM PRIORIDADE PARA CRIANÇAS COM 5 ANOS MOMENTÂNEAMENTE)
+def calcule_menor (lista):
+    menor=lista[0]
+    for elem in lista:
+        if elem<menor:
+            menor=elem
+    return menor
 
-idade = eval (input (' qual a idade da criança?'))
-if idade < 5:
- print('a criança NÃO precisa ser vacinada por enquanto')
-elif idade ==5:
-print ( 'a criança precisa ser vacinada, procure imediatamente um posto de saúde ')
-else:
-print(' crianças acima de 5 anos necessitam aguardar a chegada da vacina, retorne a ubs '
-      'para melhores orientações ')
-
-
-print('Obrigado ! Vacine-se')
+lista= [5,6,3,23,6,7,8,4]
+menor_elem =calcule_menor(lista)
+print(f'O menor valor da lista é : {menor_elem}')
+'''a,b = 5,10
+print (a*b)'''
